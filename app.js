629f51e6618c647ba5b8428486a985c813d16c50
Administrator <admin@gcpgitlab.tk>
2018-05-24 22:07:04 +0500

    added edit view and fill the text areas with the text

--- app.js
+++ app.js
@@ -64,6 +64,19 @@ app.get('/ideas/add', (req,res) => {
     res.render('ideas/add');
 });
 
+//Edit idea form
+app.get('/ideas/edit/:id',(req,res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        res.render('ideas/edit',{
+            idea
+        });
+    })
+    
+});
+
 //Process form
 
 app.post('/ideas', (req,res) => {
