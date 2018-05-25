c157b7d81a090c9120b6823e643d9a2fda31fbc2
Administrator <admin@gcpgitlab.tk>
2018-05-25 14:47:54 +0500

    added flash messages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 
 
@@ -29,6 +31,26 @@ app.use(bodyParser.json());
 
 //method override middleware
 app.use(methodOverride('_method'));
+
+
+//setting up express-session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+//setting up flash middleware
+app.use(flash());
+
+//global variables middleware
+app.use(function(req,res,next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //testing middle ware stuff
 // app.use(function(req,res,next){
