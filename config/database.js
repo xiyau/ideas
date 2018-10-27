e1dec96dd43c309642b6a085cd0c3ed3f998f2a5
Mohamed Ziyau <34277561+xiyau@users.noreply.github.com>
2018-10-27 16:11:59 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {
-        mongoURI: 'mongodb://hackode:Sndvol32@@ds249372.mlab.com:49372/idea-prod'
+        mongoURI: ''
     }
 }else{
     module.exports = {
